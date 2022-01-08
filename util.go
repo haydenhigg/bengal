0b8b0adf139ea97b9811450b93aa166af0675c19
Hayden Higginbotham <hayden@higg.me>
2022-01-08 11:01:36 -0500

    remove obsoleted functions

--- util.go
+++ util.go
@@ -1,25 +1,21 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-
 package bengal
 
 import "math"
 
 func unique(arr []string) []string {
-	uniqueWords := make(map[string]struct{})
+	uniqueTokens := make(map[string]struct{})
 
-	for _, k := range arr {
-		if _, ok := uniqueWords[k]; !ok {
-			uniqueWords[k] = struct{}{}
+	for _, v := range arr {
+		if _, ok := uniqueTokens[v]; !ok {
+			uniqueTokens[v] = struct{}{}
 		}
 	}
 
-	keys := make([]string, len(uniqueWords))
+	keys := make([]string, len(uniqueTokens))
 	i := 0
 
-	for k := range uniqueWords {
-		keys[i] = k
+	for token := range uniqueTokens {
+		keys[i] = token
 		i++
 	}
 
@@ -31,26 +27,6 @@ func flatten2d(mat [][]string) []string {
 
 	for _, r := range mat {
 		ret = append(ret, r...)
-	}
-
-	return ret
-}
-
-func getColumn(mat [][]string, index int) []string {
-	ret := make([]string, len(mat))
-
-	for i, r := range mat {
-		ret[i] = r[index]
-	}
-
-	return ret
-}
-
-func sum(arr []int) int {
-	ret := 0
-
-	for _, v := range arr {
-		ret += v
 	}
 
 	return ret
